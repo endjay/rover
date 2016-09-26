584b469975be608124f344edd426f3dabd7f7cc9
gusti amanda <gustijayaamanda@gmail.com>
2016-09-26 13:25:21 +0700

    update set class rover and method position

--- src/main/java/org/endjay/rover/Rover.java
+++ src/main/java/org/endjay/rover/Rover.java
@@ -4,5 +4,11 @@ package org.endjay.rover;
  * Created by gustiamanda on 9/26/16.
  */
 public class Rover {
-    
+
+    public void main(String[] args){
+
+    }
+    public  void position (Integer x, Integer y, Integer facing ){
+        System.out.println(x +" ,"+  y +","+  facing);
+    }
 }

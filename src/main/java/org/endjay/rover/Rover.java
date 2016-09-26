a9feb3ba1e0422f76b22b136e95c7ab3d7c87ca6
gusti amanda <gustijayaamanda@gmail.com>
2016-09-27 00:16:46 +0700

    create move and turn method

--- src/main/java/org/endjay/rover/Rover.java
+++ src/main/java/org/endjay/rover/Rover.java
@@ -7,43 +7,60 @@ import java.util.Scanner;
 public class Rover {
 
 
+    public  static  Integer xLimit = 0;
+    public  static Integer yLimit = 0;
+
     public static Integer  xAxis  = 0;
     public  static Integer yAxis = 0;
 
     public  static String direction = "N";
 
     public static void main(String[] args){
+        Rover rover = new Rover();
+        rover.setPLateuLimit();
+        rover.setRoverPosition();
+        rover.setRoverCommand();
+
+
+        System.out.format("The value of i is: %d,%d,%s", rover.xAxis,rover.yAxis,rover.direction);
+
+    }
+
+
+    public void setPLateuLimit(){
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter Plateu limit : ");
-        Integer xLimit = reader.nextInt();
-        Integer yLimit = reader.nextInt();
-        System.out.format("%d and %d",xLimit,yLimit);
+        xLimit = reader.nextInt();
+        yLimit = reader.nextInt();
+    }
 
-        //Rover.setRoverPosition(1,2,1);
-        //Rover.turn("L");
-        //System.out.format("The value of i is: %d,%d,%s", xAxis,yAxis,direction);
+    public  void setRoverPosition()  {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Set Rover Launch Position : ");
+        xAxis = reader.nextInt();
+        yAxis = reader.nextInt();
+        direction = reader.next();
 
     }
 
-
-
-    public static void setRoverPosition(Integer x, Integer y, Integer facing)  {
-
-        if(facing == 1 ){
-            direction = "N";
-        }else if( facing == 2){
-            direction = "S";
-        }else if(facing == 3 ){
-            direction = "E";
-        }else if( facing == 4){
-            direction = "W";
+    public   void setRoverCommand (){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Upload Command: ");
+        String command = reader.next();
+        String[] splitedCommand = command.split("");
+        for (int i=0; i<splitedCommand.length; i++)
+        {
+            if(splitedCommand[i] == "L"){
+                turn(splitedCommand[i]);
+            }else if(splitedCommand[i] == "M"){
+                move();
+            }
         }
 
-        xAxis = x;
-        yAxis = y;
+
     }
 
-    public static void turn(String command){
+    public  void turn(String command){
         String currentDirection = direction;
 
         if(currentDirection == "N" && command == "L"){
@@ -65,18 +82,16 @@ public class Rover {
         }
     }
 
-    public void move(){
-        Integer currentX = xAxis;
-        Integer currentY = yAxis;
+    public  void move(){
 
-        if(this.direction == "N"){
+        if(direction == "N"){
             yAxis += 1;
-        }else if(this.direction == "S"){
+        }else if(direction == "S"){
             yAxis -= 1;
-        }else if(this.direction == "E"){
+        }else if(direction == "E"){
             xAxis += 1;
 
-        }else if(this.direction == "W"){
+        }else if(direction == "W"){
             xAxis -= 1;
         }
 
